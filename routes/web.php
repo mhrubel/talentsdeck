cc0cc46b3e387eb18ba0f2c21608905350edbd16
mhrubel <bd.mhrubel@gmail.com>
2024-09-30 02:43:30 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('static.home');
+});
+
+Route::get('/contact', function () {
+    return view('static.contact');
 });
